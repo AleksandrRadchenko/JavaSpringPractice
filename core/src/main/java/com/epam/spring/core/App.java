0f45c0d0be3ecaed0d0206686bd413b1f3eb55d9
Alexander Radchenko <partner.ar@gmail.com>
2017-10-01 23:02:21 +0300

    1.7 Finish implementing CombinedEventLogger and EventType enum

--- core/src/main/java/com/epam/spring/core/App.java
+++ core/src/main/java/com/epam/spring/core/App.java
@@ -3,36 +3,46 @@ package com.epam.spring.core;
 import com.epam.spring.core.beans.Client;
 import com.epam.spring.core.beans.Event;
 import com.epam.spring.core.loggers.EventLogger;
+import com.epam.spring.core.loggers.EventType;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Map;
+
+import static com.epam.spring.core.loggers.EventType.ERROR;
+import static com.epam.spring.core.loggers.EventType.INFO;
+
 public class App {
     private Client client;
-    private EventLogger eventLogger;
+    private Map<EventType, EventLogger> loggers;
     private static ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+    private EventLogger defaultLogger;
+//    private EventLogger defaultLogger = (EventLogger) ctx.getBean("cacheFileLogger");
 
-    public App(Client client, EventLogger eventLogger) {
+    public App(Client client, EventLogger defaultLogger, Map<EventType, EventLogger> loggers) {
         this.client = client;
-        this.eventLogger = eventLogger;
+        this.defaultLogger = defaultLogger;
+        this.loggers = loggers;
     }
 
     public static void main(String[] args) {
         App app = (App) ctx.getBean(App.class);
 
-        app.logEvent("Some event for user 1");
-//        long i = 0;
-//        while (i++ < 100000000) {
-//            double d = Math.random();
-//        }
-        app.logEvent("Some event for user 2");
-//        app.logEvent("Some event for user 3");
+        app.logEvent(INFO, "Some event for user 1");
+        app.logEvent(ERROR, "Some event for user 2");
+        app.logEvent(null, "Some event for user 3");
+
         ctx.close();
     }
 
-    private void logEvent(String msg) {
+    private void logEvent(EventType type, String msg) {
+        EventLogger logger = loggers.get(type);
+        if (logger == null) {
+            logger = defaultLogger;
+        }
         Event event = (Event) ctx.getBean("event");
         event.setMsg(msg.replaceAll(
                 client.getId(), client.getFullName()));
-        eventLogger.logEvent(event);
+        logger.logEvent(event);
     }
 }
