1e8a4aad612f0033796972ff9234d0e0bb4956bb
aleksandr_radchenko <aleksandr_radchenko@epam.com>
2017-11-14 11:24:32 +0300

    1.4 Ambiguous constructor argument types - java.text.DateFormat

--- 171113/src/main/java/com/spring/practice/Event.java
+++ 171113/src/main/java/com/spring/practice/Event.java
@@ -3,6 +3,8 @@ package com.spring.practice;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.swing.text.DateFormatter;
+import java.text.DateFormat;
 import java.util.Date;
 
 public class Event {
@@ -11,14 +13,16 @@ public class Event {
     @Getter
     private String msg;
     final private Date date;
+    DateFormat df;
 
-    public Event(Date date) {
+    public Event(Date date, DateFormat df) {
         id = (int) Math.floor(100 * Math.random());
         this.date = date;
+        this.df = df;
     }
 
     @Override
     public String toString() {
-        return "Id=" + id + ", msg=" + msg + ", date=" + date;
+        return "Id=" + id + ", date=" + df.format(date) + ", msg=" + msg;
     }
 }
